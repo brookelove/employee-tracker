81a97bf5a25912ccd5f9ef16ec1f71b611be6adc
Brooke Love <savienloveb@gmail.com>
2022-05-01 15:54:44 -0700

    adding in joins to the querry sql to join the tables together

---
+++ db/query.sql
@@ -0,0 +1,9 @@
+-- this is where we join the ids and create them together
+
+SELECT * 
+-- this is joining the department id to the roles table 
+FROM roles 
+JOIN department ON roles.department_id = department.id;
+-- this is joining the roles id to the employee table 
+FROM employee
+JOIN roles ON employee.role_id = role.id;
