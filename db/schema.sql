e46eb85d7fa2d782ea3993322daea990d45c6cb2
Brooke Love <savienloveb@gmail.com>
2022-05-01 15:27:39 -0700

    created schema with seperate tables for roles, departments, and employee

--- db/schema.sql
+++ db/schema.sql
@@ -5,3 +5,40 @@
 2. three seperate tables that will rely on each other 
 3. each has their own parameters 
 */
+-- deletes and creates a database incorperated 
+DROP DATABASE IF EXISTS incorperated_db;
+CREATE DATABASE incorperated_db;
+
+-- use this database for these tables 
+USE incorperated_db;
+
+-- creates first table of department with parameters 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL 
+);
+
+-- creates the second table it does rely on the first table for the department id 
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+-- creates the third table it does rely on the second table for the department id 
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
+);
+
+SHOW TABLES
